ad7fda956724be0570eba2639495aca361084dce
@kirilloleynik <kirill.olejnik@gmail.com>
2020-06-02 16:35:24 +0300

    Setup rubocop & make changes according to linter recommendations

--- entities/author.rb
+++ entities/author.rb
@@ -1,19 +1,25 @@
+# frozen_string_literal: true
+
+# An author of a book
 class Author
   attr_reader :name, :bio
-
   def initialize(args)
     @name = args[:name]
     @bio = args[:bio]
-    rescue TypeError
-      @name = @bio = nil
+  rescue TypeError
+    @name = @bio = nil
   end
+
   def valid?
     return name_is_valid?
   end
+
   def to_s
     name.to_s
   end
+
   private
+
   def name_is_valid?
     return name.is_a?(String) && name.length > 0
   end
